ae02881625305c5107b0be83cb5bbea6ad071b70
Jordan Cruz-Correa <jordancruz-correa@jordans-mbp.lan>
2019-11-03 22:08:27 -0500

    Completed 11

--- 11 - Custom Video Player/scripts-FINISHED.js
+++ 11 - Custom Video Player/scripts-FINISHED.js
@@ -51,5 +51,5 @@ ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
 let mousedown = false;
 progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
-progress.addEventListener('mousedown', () => mousedown = true);
-progress.addEventListener('mouseup', () => mousedown = false);
+progress.addEventListener('mousedown', () => (mousedown = true));
+progress.addEventListener('mouseup', () => (mousedown = false));
